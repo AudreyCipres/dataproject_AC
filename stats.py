ab6b89521b7135deb481cdbe28eb5376a1fbd016
Audrey Cipres <audreycipres123@gmail.com>
2022-08-18 17:17:22 +1000

    Data Project - need to work on graph management

--- stats.py
+++ stats.py
@@ -2,4 +2,13 @@ from pandas import read_json
 import json
 
 # read file
-myjsonfile = open
+myjsonfile = open(' /download/2020-biannual-incident-report.csv', 'r')
+jsondata = myjsonfile.read()
+
+# Parse
+obj = json.loads(jsondata)
+
+print(str(obj['Date/Time Opened']))
+print(str(obj['Operational Directorate']))
+print(str(obj['Primary Category']))
+print(str(obj['Summary of the Incident (External Distribution)']))
